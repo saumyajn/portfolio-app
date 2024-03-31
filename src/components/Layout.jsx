45b8f9b80185ca7639144924caa6b91451b0f3c3
Saumya Jain <saums06@gmail.com>
2024-03-30 20:09:33 -0400

    dark theme using new changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,18 +3,16 @@ import Header from "./header";
 import * as React from 'react';
 import { Grid } from "@mui/material";
 export default function Layout(props) {
-
+  
     const handleBack = (data) => {
         props.appPathCall(data)
     };
-    const handlechangeTheme = (val) => {
-        props.appChangeTheme(val)
-    }
-    return (<>
+  
+    return (<>       
         <Grid sx={{ flexGrow: 1 }}>
             <Grid container spacing={2}>
                 <Grid item xs={1} sm={3} md={3}>
-                    <Header parentPathCall={handleBack} changeTheme={handlechangeTheme} />
+                    <Header parentPathCall={handleBack} />
 
                 </Grid>
                 <Grid item xs={11} sm={9} md={9}>
